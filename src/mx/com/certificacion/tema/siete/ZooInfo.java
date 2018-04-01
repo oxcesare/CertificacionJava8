19451d1bd1dcb70dc72921667367fcc289e911c0
consultor006 <consultor006@192.168.1.70>
2018-03-31 18:45:23 -0600

    ExecutorService

---
+++ src/mx/com/certificacion/tema/siete/ZooInfo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.certificacion.tema.siete;
+
+/**
+ *
+ * @author consultor006
+ * 
+ * Clase que demuestra el uso de la interface 
+ * ExecutorService, esta nos permite crear hilos, 
+ * administrarlos, calendarizarllos con una API ya establecida y
+ * haciendo uso de programación funcional.
+ */
+public class ZooInfo {
+    
+}
