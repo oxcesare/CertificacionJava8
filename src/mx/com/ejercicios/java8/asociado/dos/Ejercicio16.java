3776a56b468b3e892a1e09b05d08cd70952b52e1
consultor006 <consultor006@192.168.1.70>
2018-04-16 19:17:16 -0500

    Uso de los métodos concat y charAt de la clase String

---
+++ src/mx/com/ejercicios/java8/asociado/dos/Ejercicio16.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.ejercicios.java8.asociado.dos;
+
+/**
+ *
+ * @author consultor006
+ * 
+ * Esta clase compila con error ya que el método charAt 
+ * regresa un char y no se puede concatenar con el 
+ * metodo concat de String.
+ */
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        Ejercicio16 q = new Ejercicio16();
+        String s = "hello wordl";
+        System.out.println(s.equals("hello wordl"));
+        System.out.println(s.concat(s.charAt(0)));
+    }
+}
