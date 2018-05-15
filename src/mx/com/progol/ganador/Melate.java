be310c77a3c9c9266d5109d1a83f17e3bba448ea
consultor006 <consultor006@192.168.1.70>
2018-05-15 06:51:37 -0500

    Actualización de ejercicios certificación y se agregan nuevos ejercicios

---
+++ src/mx/com/progol/ganador/Melate.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.progol.ganador;
+
+/**
+ *
+ * @author consultor006
+ * 
+ * Clase Melate para generar 6 números aleatorios para 
+ * Jugar Melate Semanal.
+ * 
+ * Ganar - to Win
+ */
+public class Melate {
+    public static void main(String[] args) {         
+        int numeros =0;
+        
+        for( int i=0; i<=6; i++){
+            numeros = (int) (Math.random() * 56) + 1;
+            System.out.println("Número"+" "+numeros);
+        }
+        
+    }
+}
