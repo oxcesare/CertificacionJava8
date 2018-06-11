b2c7f86e616bd163f04e469eb43a66b7b0c011af
consultor006 <consultor006@192.168.1.70>
2018-06-10 22:22:56 -0500

    Actualización de ejercicios certificación y se agregan nuevos ejercicios

---
+++ src/mx/com/certificacion/lambdas/Test8.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.certificacion.lambdas;
+
+/**
+ *
+ * @author consultor006
+ */
+public class Test8 {
+    
+    public static void main(String[] args) {
+        System.out.println("Metodo Principal");
+    }    
+}
