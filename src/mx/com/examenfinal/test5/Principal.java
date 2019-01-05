d18812df7d2c70aadcad987e0501e9306b1d1ac3
consultor006 <consultor006@192.168.1.70>
2019-01-05 10:13:28 -0600

    Enero 2019, nuevos ejemplos de la certificación

---
+++ src/mx/com/examenfinal/test5/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.examenfinal.test5;
+
+/**
+ *
+ * @author cesaralducinruiz
+ * 
+ * Día 5 de Enero 2019 
+ * 
+ * Se tiene que hacer minimo un simulador por día 
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/mx/com/examenfinal/test5/Test5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.examenfinal.test5;
+
+/**
+ *
+ * @author cesaralducinruiz
+ */
+public class Test5 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
