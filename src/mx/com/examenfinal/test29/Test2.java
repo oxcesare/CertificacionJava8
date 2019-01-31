e2c6de31e5d80aed46c19282c15caeb88206d019
consultor006 <consultor006@192.168.1.70>
2019-01-31 09:21:32 -0600

    Ejercicios 2019

---
+++ src/mx/com/examenfinal/test29/Test2.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.examenfinal.test29;
+
+
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+
+import java.time.LocalTime;
+
+import java.time.Period;
+
+import java.time.Duration;
+
+
+
+/**
+ *
+ * @author cesaralducinruiz
+ * 
+ * En la Version Java 8, 
+ * 
+ * Nos encontramos con una nueva forma de invocar los Dates, Times, Locales,
+ * y Resource Bundle.
+ * 
+ * Resource Bundle consiste en crear archivos tipo properties para la configuraciones
+ * regionales.
+ * 
+ * Tambien se incorpora el concepto de Periodo y Duration.
+ * 
+ * Periodo: dias, meses y años
+ * 
+ * Duration:, horas, minutos y segundos.
+ * 
+ * Instant
+ * 
+ * TemporalUnit.
+ * 
+ * 
+ * 
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/mx/com/examenfinal/test30/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.examenfinal.test30;
+
+/**
+ *
+ * @author cesaralducinruiz
+ * 
+ * Día 30 de Enero 2019 
+ * 
+ * Se tiene que hacer minimo un simulador por día 
+ */
+public class Principal {
+    
+}
